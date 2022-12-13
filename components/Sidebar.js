3df33d6eb114567db120d1da75c41bcda549b202
fellgar246 <fellgar246@gmail.com>
2022-12-13 14:41:48 -0600

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import useQuiosco from '../hooks/useQuiosco';
+import Categoria from "./Categoria";
+
+
+const Sidebar = () => {
+  const { categorias } = useQuiosco();
+
+  return (
+    <>
+        <Image
+            width={150}
+            height={70}
+            src="/assets/img/logo.svg"
+            alt="imagen logotipo"
+        />
+       
+
+        <nav className="mt-10">
+          {categorias.map(categoria => (
+            <Categoria 
+              key={categoria.id}
+              categoria={categoria}
+            />
+          ))}
+        </nav>
+    </>
+  )
+}
+
+export default Sidebar;
